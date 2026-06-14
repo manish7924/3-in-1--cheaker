77a697990bab0e46d8ca495074f36737e22b30c5
Ir⎊nic7 <114106502+manish7924@users.noreply.github.com>
2026-06-14 19:11:41 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+TOKEN = "" # Insert your token here
+OWNER_ID = 7171372952
+CHANNEL_ID = -1002746651949
+CHANNEL_LINK = "https://t.me/+-3aQeXQ483g4NWFl"
+STATUS_FILE = "bot_status.txt"
