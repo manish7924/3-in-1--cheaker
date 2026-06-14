7463ad9939f5815c5b5cf9352a0fbca5e74b2fc0
Ir⎊nic7 <114106502+manish7924@users.noreply.github.com>
2026-06-14 19:23:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-TOKEN = "" # Insert your token here
+TOKEN = "8545283269:AAFwrsMPUhXZ_-jFBHOXXfNWXWmV6OH_JyE" # Insert your token here
 OWNER_ID = 7171372952
 CHANNEL_ID = -1002746651949
 CHANNEL_LINK = "https://t.me/+-3aQeXQ483g4NWFl"
